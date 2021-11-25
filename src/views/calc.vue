f1364869433f215685732bf600fd845ad5b2c91c
StepkaP <Steve6305@gmail.com>
2021-11-25 17:51:15 +0700

    Fix unary error output

--- src/views/calc.vue
+++ src/views/calc.vue
@@ -74,7 +74,7 @@ export default {
         this.output = `${operator}(${inputValue}) =`;
         this.addToHistory([this.output, result].join(" "));
       } catch (e) {
-        console.log(e.message);
+        console.log(e);
         this.output = "Error";
       }
     },
