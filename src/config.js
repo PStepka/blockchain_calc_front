ec38403bb21f6069175c8142ef9800733c64c950
StepkaP <Steve6305@gmail.com>
2022-01-18 19:54:39 +0700

    testing mexa

--- src/config.js
+++ src/config.js
@@ -11,5 +11,6 @@ export const config = {
     UNARY_OPERATORS: {
         "invert": 0,
         "sqrt": 1
-    }
+    },
+    contractAddress: '0x09F5e6b9fF037Bbee46C6017e5e9fc4E0ce04508'
 }
--- src/views/calc.vue
+++ src/views/calc.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <textarea rows="3" class="history display" v-model="history" disabled="disabled"/><br>
     <input class="display" v-model="output" disabled="disabled"><br>
-      <input ref="input" class="display" v-model="input" @keypress="onInput"><br>
+      <input ref="input" class="display" v-model="input"><br>
     <div>
       <button class="calc-button" @click="setBinaryOperator('+')">+</button>
       <button class="calc-button" @click="setBinaryOperator('-')">-</button>
@@ -24,40 +24,78 @@
 <script>
 import { config } from '@/config';
 import { ethers } from 'ethers';
+import {Biconomy} from "@biconomy/mexa"
 const contract = require('./contract.json');
+
+const domainType = [
+  { name: "name", type: "string" },
+  { name: "version", type: "string" },
+  { name: "chainId", type: "uint256" },
+  { name: "verifyingContract", type: "address" },
+];
+
+const metaTransactionType = [
+  { name: "nonce", type: "uint256" },
+  { name: "from", type: "address" },
+  { name: "functionSignature", type: "bytes" }
+];
+
+let domainData = {
+  name: "mycalc",
+  version: "1",
+  chainId: 80001,
+  verifyingContract: config.contractAddress,
+};
+
+let biconomy = undefined;
 
 export default {
   name: 'Calc',
   async mounted() {
-    // A Web3Provider wraps a standard Web3 provider, which is
-    // what MetaMask injects as window.ethereum into each page
+    if (!window.ethereum) {
+      console.log("No window.ethereum obj");
+    }
+
     await window.ethereum.enable();
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    biconomy = new Biconomy(window.ethereum, {walletProvider: window.ethereum, apiKey: 'lOwZJ8ivy.e0dca552-803f-4c01-9294-6ccdf981b9c0', strictMode: true});
+    this.ethersProvider = new ethers.providers.Web3Provider(biconomy);
+    this.walletProvider = new ethers.providers.Web3Provider(window.ethereum);
+    const walletSigner = this.walletProvider.getSigner();
 
-    console.log(await provider.listAccounts());
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
-    const signer = provider.getSigner();
-    const balance = await provider.getBalance("0x9F327c8554B5ADB5eD5d9051Ecd91CfC1eac409a");
+    if (!walletSigner) {
+      console.log("No wallet signer");
+    }
 
-    console.log(balance.toString());
-    console.log(ethers.utils.formatEther(balance));
+    this.selectedAddress = (await walletSigner.getAddress()).toLowerCase();
+    console.log("SELECTED ADDRESS", this.selectedAddress);
 
-    const contractAddress = "0x908609443767cf83d2cA39c86A89F60a11EDC19E";
-    const contractAbi = contract.output.abi;
+    biconomy.onEvent(biconomy.READY, async() => {
+      console.log("Biconomy is ready");
 
-    const contr = new ethers.Contract(contractAddress, contractAbi, provider);
+      const contractAddress = config.contractAddress;
+      console.log(contractAddress);
+      const contractAbi = contract.output.abi;
 
-    console.log((await contr.get()).toString());
+      this.contrWithSigner = new ethers.Contract(contractAddress,
+          contractAbi, biconomy.getSignerByAddress(this.selectedAddress));
 
-    const contrWithSigner = contr.connect(signer);
-    this.contrWithSigner = contrWithSigner;
+      console.log(this.contrWithSigner);
+      this.contractInterface = new ethers.utils.Interface(contractAbi);
+    })
+
 
   },
   data() {
     return {
+      newQuote: undefined,
+      transactionHash: undefined,
+      metaTxEnabled: true,
+      selectedAddress: undefined,
+      walletProvider: undefined,
+      ethersProvider: undefined,
+      quote: undefined,
+      owner: undefined,
       historyList: [],
       input: "",
       output: "",
@@ -65,27 +103,90 @@ export default {
       secondOperand: undefined,
       operator: undefined,
       result: 0,
-      contrWithSigner: null
+      contrWithSigner: undefined,
+      contractInterface: undefined,
+
     }
   },
   methods: {
-    async onInput ($event) {
-      const key = $event.key;
-      if (config.BINARY_OPERATORS[key] !== undefined) {
-        $event.preventDefault();
-        await this.setBinaryOperator(key);
-        return;
+    getSignatureParameters(signature) {
+      if (!ethers.utils.isHexString(signature)) {
+        throw new Error(
+            'Given value "'.concat(signature, '" is not a valid hex string.')
+        );
       }
+      var r = signature.slice(0, 66);
+      var s = "0x".concat(signature.slice(66, 130));
+      var v = "0x".concat(signature.slice(130, 132));
+      v = ethers.BigNumber.from(v).toNumber();
+      if (![27, 28].includes(v)) v += 27;
+      return {
+        r: r,
+        s: s,
+        v: v
+      };
+    },
+    async onSubmitWithEIP712Sign() {
+      console.log("selectedAddress:", this.selectedAddress);
+      if (this.contrWithSigner) {
+        this.transactionHash = "";
+        if (this.metaTxEnabled) {
+          console.log(`Getting user signature`);
+          const nonce = await this.walletProvider.getTransactionCount(this.selectedAddress);
+          console.log("nonce 1 = ", nonce);
+          const functionSignature = this.contractInterface.encodeFunctionData("store", [ parseInt(this.input)]);
+          console.log("functionSignature", functionSignature);
+          const message = {};
+          message.nonce = parseInt(nonce).toString();
+          message.from = this.selectedAddress;
+          message.functionSignature = functionSignature;
 
-      if (key === "=") {
-        $event.preventDefault();
-        await this.calculate();
-        return;
+          const dataToSign = JSON.stringify({
+            types: {
+              EIP712Domain: domainType,
+              MetaTransaction: metaTransactionType
+            },
+            domain: domainData,
+            primaryType: "MetaTransaction",
+            message: message
+          });
+
+          // Its important to use eth_signTypedData_v3 and not v4 to get EIP712 signature because we have used salt in domain data
+          // instead of chainId
+          let signature = await this.walletProvider.send("eth_signTypedData_v4", [this.selectedAddress, dataToSign]);
+          console.log("data signature:", signature);
+          let { r, s, v } = this.getSignatureParameters(signature);
+          console.log("r s v = ", r, s, v);
+          await this.sendSignedTransaction(this.selectedAddress, functionSignature, r, s, v);
+        } else {
+          console.log("Sending normal transaction");
+          let tx = await this.contrWithSigner.store(this.input);
+          console.log("Transaction hash : ", tx.hash);
+          console.log(`Transaction sent by relayer with hash ${tx.hash}`);
+          let confirmation = await tx.wait();
+          console.log(confirmation);
+          this.transactionHash = tx.hash;
+
+          console.log("Transaction confirmed on chain");
+        }
+      } else {
+        console.log("Not initialized yet");
       }
+    },
+    async sendSignedTransaction(userAddress, functionData, r, s, v) {
+      try {
+        console.log(`Sending transaction via Biconomy`);
+        let tx = await this.contrWithSigner.executeMetaTransaction(userAddress, functionData, r, s, v);
+        console.log(`Transaction sent. Waiting for confirmation ..`)
+        await tx.wait(1);
+        console.log("Transaction hash : ", tx.hash);
+        //let confirmation = await tx.wait();
+        console.log(tx);
+        this.transactionHash = tx.hash;
 
-      const keyCode = ($event.keyCode ? $event.keyCode : $event.which);
-      if (keyCode < 48 || keyCode > 57) {
-        $event.preventDefault();
+        console.log("Transaction confirmed on chain");
+      } catch (error) {
+        console.log(error);
       }
     },
     async useUnaryOperator(operator) {
@@ -195,11 +296,18 @@ export default {
       }
     },
     async store() {
-      const input = parseInt(this.input);
 
-      const tx = this.contrWithSigner.store(input);
+      await this.onSubmitWithEIP712Sign();
 
-      await tx;
+      // const tx = this.contrWithSigner.store(input);
+      // const old = this.input;
+      // this.input = "Please wait...";
+      //
+      // await tx;
+      //
+      // this.input = old;
+
+
     },
     async get() {
       const requestOptions = {
